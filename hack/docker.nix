b4a4870e966c5897aa6b8906445fc3fdbce21038
Terin Stock <terinjokes@gmail.com>
2021-03-17 18:32:16 -0700

    chore(docker): add Docker Hub publishing
    Add a GitHub Action to build and publish Docker Hub images
    on tagged releases.
    
    Fixes: #1

--- hack/docker.nix
+++ hack/docker.nix
@@ -1,6 +1,13 @@
-{ config, lib, pkgs, controller, ... }:
+ { pkgs ? import ./nixpkgs.nix { }, ... }:
 
-pkgs.dockerTools.buildLayeredImage {
-  name = "lockbox-controller";
-  config.Entrypoint = [ "${controller}/bin/lockbox-controller" ];
-}
+ with pkgs;
+
+ let package = callPackage ./derivation.nix { };
+
+ in dockerTools.buildLayeredImage {
+   name = "lockbox-controller";
+   config = {
+     Entrypoint = [ "${package.lockbox}/bin/lockbox-controller" ];
+     Env = [ "NIX_SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt" ];
+   };
+ }
--- shell.nix
+++ shell.nix
@@ -1,14 +1,5 @@
-{ pkgs ? import (builtins.fetchTarball {
-  name = "nixos-2009-2020-11-06";
-  url = "https://github.com/NixOS/nixpkgs/archive/d105075a1fd870b1d1617a6008cb38b443e65433.tar.gz";
-  sha256 = "1jcs44wn0s6mlf2jps25bvcai1rij9b2dil6zcj8zqwf2i8xmqjh";
-}) {} }:
+{ pkgs ? import ./hack/nixpkgs.nix { }, ... }:
 
 with pkgs;
 
-mkShell {
-  buildInputs = [
-    go
-    goimports
-  ];
-}
+mkShell { buildInputs = [ go goimports ]; }
