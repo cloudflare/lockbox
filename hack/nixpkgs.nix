61db2e25fb6339f297c970d83616812e1e996316
Terin Stock <terinjokes@gmail.com>
2021-03-17 18:32:16 -0700

    chore(docker): add Docker Hub publishing
    Add a GitHub Action to build and publish Docker Hub images
    on tagged releases.
    
    Fixes: #1

---
+++ hack/nixpkgs.nix
@@ -0,0 +1,6 @@
+import (builtins.fetchTarball {
+  name = "nixos-unstable-2021-03-16";
+  url =
+    "https://github.com/NixOS/nixpkgs/archive/266dc8c3d052f549826ba246d06787a219533b8f.tar.gz";
+  sha256 = "09ydqx2lznixmw8z4cfz1j3k137mh8n3cdpygwqymknhfdjq7lg4";
+})
