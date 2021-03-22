e568d508fc7b4b06b3a4403dfc46e185fd1d9a6a
Terin Stock <terinjokes@gmail.com>
2021-03-22 14:45:18 -0700

    release: v0.5.0

--- hack/derivation.nix
+++ hack/derivation.nix
@@ -1,12 +1,13 @@
 { config, lib, pkgs, ... }:
 
-let srcs = lib.sourceFilesBySuffices ../. [".go" ".mod" ".sum"];
-    v256 = "0n8k2syjv2b1fd6w3rkq2lm8kdyyj0f8dqprxarvx1pywdznamvf";
+let
+  srcs = lib.sourceFilesBySuffices ../. [ ".go" ".mod" ".sum" ];
+  v256 = "0n8k2syjv2b1fd6w3rkq2lm8kdyyj0f8dqprxarvx1pywdznamvf";
 
 in {
   lockbox = pkgs.buildGoModule rec {
     pname = "lockbox";
-    version = "0.0.0";
+    version = "0.5.0";
 
     src = srcs;
 
@@ -16,13 +17,13 @@ in {
   };
   locket = pkgs.buildGoModule rec {
     pname = "locket";
-    version = "0.0.0";
+    version = "0.5.0";
 
     src = srcs;
-    buildFlagsArray = [ "-ldflags=" "-X=main.version=${version}"];
+    buildFlagsArray = [ "-ldflags=" "-X=main.version=${version}" ];
 
     vendorSha256 = v256;
 
-    subPackages = ["cmd/locket"];
+    subPackages = [ "cmd/locket" ];
   };
 }
