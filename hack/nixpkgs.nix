0abfaa889fede3d04a6a947c50a1c452f80ca0ce
Terin Stock <terinjokes@gmail.com>
2023-12-22 18:46:58 +0000

    chore: migrate to standard build
    Adapts the team's standard Makefile to use on GitHub, and migrates the
    controller's Docker base image library/golang and distroless.

--- hack/nixpkgs.nix
+++
@@ -1,6 +0,0 @@
-import (builtins.fetchTarball {
-  name = "nixos-unstable-2022-05-12";
-  url =
-    "https://github.com/NixOS/nixpkgs/archive/aa2f845096f72dde4ad0c168eeec387cbd2eae04.tar.gz";
-  sha256 = "0l732ci2g78pcgk9kqn6c18h4j47dhp1dys52cmqhzm4pyi6dl0z";
-})
